4b18655384d65235240b2344f2c0693292d68546
Silvano Junior <silvano.marini.dev@gmail.com>
2026-01-16 17:39:30 -0300

    feat: configure global templates with DIRS and template inheritance

--- django_learning_lab/django_learning_lab/settings.py
+++ django_learning_lab/django_learning_lab/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'django_learning_lab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+        BASE_DIR / 'base'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
