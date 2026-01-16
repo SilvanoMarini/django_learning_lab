77cf7cdabb0d8f828a9127d612ebe4e50fce1374
Silvano Junior <silvano.marini.dev@gmail.com>
2026-01-16 17:04:39 -0300

    feat: add templates and configure Home and Blog apps

--- django_learning_lab/blog/views.py
+++ django_learning_lab/blog/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def blog(request):
     print('blog')
-    return HttpResponse('blog do app 1')
+    return render(request=request, template_name='blog/index.html')
 
 def exemplo(request):
     print('exemplo')
-    return HttpResponse('exemplo do app 1')
+    return render(request=request, template_name='blog/exemplo.html')
