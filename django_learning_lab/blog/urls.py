152e36bf101db304f0ac25870874c688c3582344
Silvano Junior <silvano.marini.dev@gmail.com>
2026-01-21 12:19:45 -0300

    feat: add dynamic URL routing and template usage

--- django_learning_lab/blog/urls.py
+++ django_learning_lab/blog/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views
 
-
+app_name = 'blog'
 
 
 urlpatterns = [
-    path('', views.blog),
-    path('example/', views.example),
+    path('', views.blog, name='home'),
+    path('example/', views.example, name='example'),
 ]
